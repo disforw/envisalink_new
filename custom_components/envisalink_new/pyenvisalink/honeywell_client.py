0b84eb1abd71b334bfc066190ffe94ccda8948ce
disforw <github@benzona.net>
2023-05-07 13:42:24 -0400

    Add keypresses_to_partition

--- custom_components/envisalink_new/pyenvisalink/honeywell_client.py
+++ custom_components/envisalink_new/pyenvisalink/honeywell_client.py
@@ -113,6 +113,9 @@ class HoneywellClient(EnvisalinkClient):
         """Public method to raise a panic alarm."""
         await self.keypresses_to_partition(1, evl_PanicTypes[panicType])
 
+    async def toggle_chime(self, code):
+        await self.keypresses_to_partition(1, "%s9" % code)
+
     def parseHandler(self, rawInput):
         """When the envisalink contacts us- parse out which command and data."""
         cmd = {}
